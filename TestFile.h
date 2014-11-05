57fd0c24bd6d860faf2229fb8aacaea995bde724
shreyaclabs <shreya@clicklabs.in>
2014-11-05 16:42:20 +0530

    commit1

---
+++ TestFile.h
@@ -0,0 +1,13 @@
+//
+//  TestFile.h
+//  XBowling 3.0
+//
+//  Created by Click Labs on 11/5/14.
+//  Copyright (c) 2014 Click Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestFile : UIView
+
+@end
---
+++ TestFile.m
@@ -0,0 +1,13 @@
+//
+//  TestFile.m
+//  XBowling 3.0
+//
+//  Created by Click Labs on 11/5/14.
+//  Copyright (c) 2014 Click Labs. All rights reserved.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+@end
